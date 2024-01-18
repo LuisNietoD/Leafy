ed4698c379e4210c946d50f97e6ed58b04b9f445
Dandelikeswood <j.boudouresque@gmail.com>
2024-01-18 16:49:31 +0100

    Scriptable Object des cartes

---
+++ Assets/Scenes/JAJA/Scripts/ScriptableCard.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Card_Type
+{
+    energies,
+    resources,
+    interaction,
+    activator,
+    storage,
+    interfaceCard,
+    autoCard,
+    tools
+}
+
+[CreateAssetMenuAttribute(fileName = "Scriptable Card", menuName = "My Game/ Scriptable Card")]
+public class ScriptableCard : ScriptableObject
+{
+    public new string name;
+    public Sprite artwork;
+    public Color background_color;
+    public int price;
+    public bool sellable;
+    public List<GameObject> craft_recipe;
+    public Card_Type type;
+}
