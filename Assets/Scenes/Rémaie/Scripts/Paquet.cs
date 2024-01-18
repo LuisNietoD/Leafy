aec5d73acb771a73a218f1be8503fba924d54e1e
Zectalia <95757705+Zectalia@users.noreply.github.com>
2024-01-18 19:15:16 +0100

    Paquet affichage disponible dynamique

--- Assets/Scenes/Rémaie/Scripts/Paquet.cs
+++ Assets/Scenes/Rémaie/Scripts/Paquet.cs
@@ -7,11 +7,18 @@ using TMPro;
 [ExecuteInEditMode]
 public class CartesManager : MonoBehaviour
 {
-    public float jetons = 50; // Le nombre de jetons que le joueur possède
-    public TextMeshProUGUI consoleText; // Référence au Text UI pour afficher les résultats dans la console
-    public TextMeshProUGUI cartesRaresText; // Text UI pour afficher le nombre de cartes rares obtenues
-    public GameObject[] prefabsCartes; // Tableau de préfabs pour chaque carte
-    public float distanceSpawnDevantCamera = 5f; // Définissez la distance à laquelle les objets seront spawnés devant la caméra
+    public float jetons = 50;
+    public TextMeshProUGUI consoleText;
+    public TextMeshProUGUI cartesRaresText;
+    public GameObject[] prefabsCartes;
+    public float distanceSpawnDevantCamera = 5f;
+    public RawImage Mask1;
+    public RawImage Mask2;
+    public RawImage Mask3;
+    public RawImage Mask4;
+    public RawImage Mask5;
+    public RawImage Mask6;
+    public RawImage Mask7;
 
     // Liste de cartes différentes
     private List<string> cartes = new List<string>()
@@ -25,22 +32,36 @@ public class CartesManager : MonoBehaviour
         "Carte Violette",
     };
 
-    // Tableau de probabilités associé à chaque couleur de carte
-    [SerializeField] private float[] probabilitesCouleurs = { 0.5f, 0.12f, 0.12f, 0.12f, 0.12f, 0.06f, 0.06f }; // Modifiez ces valeurs selon vos besoins
+    [SerializeField] private float[] probabilitesCouleurs = { 0.5f, 0.12f, 0.12f, 0.12f, 0.12f, 0.06f, 0.06f };
+    [SerializeField][Range(0.0f, 1.0f)] private float seuilRarete = 0.06f;
+    private int nombrePaquetsOuverts = 0;
+    private int nombreCartesRares = 0;
 
-    [SerializeField]
-    [Range(0.0f, 1.0f)] // Définir la plage de valeurs autorisées entre 0.0 et 1.0
-    private float seuilRarete = 0.06f; // Valeur qui définit qu'une carte est rare
+    // Ajoute une classe CustomMask pour inclure l'état de destruction
+    [System.Serializable]
+    public class CustomMask
+    {
+        public RawImage mask;
+        public bool destroyed;
+    }
 
-    private int nombrePaquetsOuverts = 0; // Variable pour stocker le nombre de paquets ouverts
-    private int nombreCartesRares = 0; // Variable pour stocker le nombre de cartes rares obtenues
+    // Déclare un dictionnaire pour mapper le nom de la carte à la CustomMask correspondante
+    private Dictionary<string, CustomMask> carteToMask = new Dictionary<string, CustomMask>();
 
-    // Fonction appelée lorsque le joueur appuie sur le bouton
+    void Start()
+    {
+        // Ajoute chaque carte et sa CustomMask au dictionnaire
+        carteToMask.Add("Carte Bleu", new CustomMask { mask = Mask1 });
+        carteToMask.Add("Carte Jaune", new CustomMask { mask = Mask2 });
+        carteToMask.Add("Carte Orange", new CustomMask { mask = Mask3 });
+        carteToMask.Add("Carte Rose", new CustomMask { mask = Mask4 });
+        carteToMask.Add("Carte Rouge", new CustomMask { mask = Mask5 });
+        carteToMask.Add("Carte Vert", new CustomMask { mask = Mask6 });
+        carteToMask.Add("Carte Violette", new CustomMask { mask = Mask7 });
+    }
 
+    public TextMeshProUGUI infoCartesText;
 
-    public TextMeshProUGUI infoCartesText; // Référence au Text UI pour afficher les informations sur les cartes
-
-    // Fonction appelée lorsque la souris entre dans la zone du bouton
     public void AfficherCartesPossibles()
     {
         string infoCartes = "Cartes possibles :\n";
@@ -50,7 +71,6 @@ public class CartesManager : MonoBehaviour
             infoCartes += carte + "\n";
         }
 
-        // Affiche les informations sur les cartes dans le Text UI dédié
         if (infoCartesText != null)
         {
             infoCartesText.text = infoCartes;
@@ -61,10 +81,8 @@ public class CartesManager : MonoBehaviour
         }
     }
 
-    // Fonction appelée lorsque la souris quitte la zone du bouton
     public void CacherCartesPossibles()
     {
-        // Efface le texte d'information sur les cartes
         if (infoCartesText != null)
         {
             infoCartesText.text = "";
@@ -74,28 +92,23 @@ public class CartesManager : MonoBehaviour
             Debug.LogError("Text UI non assigné pour afficher les informations sur les cartes.");
         }
     }
+
     public void ObtenirCartes()
     {
-        if (jetons >= 5f) // Vérifie si le joueur a assez de jetons
+        if (jetons >= 5f)
         {
-            // Détruit les cartes existantes
             DetruireCartesExistants();
 
-            jetons -= 5f; // Déduit le coût en jetons
+            jetons -= 5f;
 
-            // Obtient 3 cartes aléatoires
             List<string> cartesObtenues = ObtenirCartesAleatoires(3);
 
-            // Incrémente le nombre de paquets ouverts
             nombrePaquetsOuverts++;
 
-            // Affiche les cartes, le nombre de paquets ouverts et le nombre de jetons dans l'UI
             AfficherCartesEtInfoDansUI(cartesObtenues);
 
-            // Fait apparaître les objets correspondant aux cartes tirées
             CreerObjetsCartes(cartesObtenues);
 
-            // Vérifie si des cartes rares ont été obtenues et met à jour le compteur
             VerifierCartesRares(cartesObtenues);
         }
         else
@@ -104,7 +117,6 @@ public class CartesManager : MonoBehaviour
         }
     }
 
-    // Fonction pour obtenir un nombre donné de cartes aléatoires en fonction des probabilités
     private List<string> ObtenirCartesAleatoires(int nombreDeCartes)
     {
         List<string> cartesAleatoires = new List<string>();
@@ -113,11 +125,10 @@ public class CartesManager : MonoBehaviour
 
         for (int i = 0; i < nombreDeCartes; i++)
         {
-            float randomValue = (float)random.NextDouble(); // Génère une valeur aléatoire entre 0 et 1
+            float randomValue = (float)random.NextDouble();
 
             float cumulativeProbability = 0f;
 
-            // Sélectionne une carte en fonction des probabilités
             for (int j = 0; j < cartes.Count; j++)
             {
                 cumulativeProbability += probabilitesCouleurs[j];
@@ -133,28 +144,20 @@ public class CartesManager : MonoBehaviour
         return cartesAleatoires;
     }
 
-    // Fonction pour afficher les cartes, le nombre de paquets ouverts et le nombre de jetons dans l'UI
     private void AfficherCartesEtInfoDansUI(List<string> cartesObtenues)
     {
-        // Vérifie s'il y a un Text UI assigné
         if (consoleText != null)
         {
-            // Efface le texte existant
             string nouveauTexte = "Cartes obtenues : ";
 
-            // Ajoute les cartes obtenues au Text UI
             foreach (string carte in cartesObtenues)
             {
                 nouveauTexte += "\n" + carte;
             }
 
-            // Ajoute le nombre actuel de paquets ouverts au Text UI
             nouveauTexte += "\n\nPaquets ouverts : " + nombrePaquetsOuverts;
-
-            // Ajoute le nombre actuel de jetons au Text UI
             nouveauTexte += "\n\nJetons restants : " + jetons;
 
-            // Réassigne le texte
             consoleText.text = nouveauTexte;
         }
         else
@@ -163,20 +166,16 @@ public class CartesManager : MonoBehaviour
         }
     }
 
-    // Fonction pour vérifier les cartes rares et mettre à jour le compteur
     private void VerifierCartesRares(List<string> cartesObtenues)
     {
-        // Vérifie s'il y a des cartes rares dans la liste obtenue
         foreach (string carte in cartesObtenues)
         {
             if (probabilitesCouleurs[cartes.IndexOf(carte)] == seuilRarete)
             {
-                // Incrémente le nombre de cartes rares
                 nombreCartesRares++;
             }
         }
 
-        // Affiche le nombre de cartes rares dans l'UI
         if (cartesRaresText != null)
         {
             cartesRaresText.text = "Cartes rares obtenues : " + nombreCartesRares;
@@ -187,10 +186,8 @@ public class CartesManager : MonoBehaviour
         }
     }
 
-    // Fonction pour créer des objets correspondant aux cartes tirées
     private void CreerObjetsCartes(List<string> cartesObtenues)
     {
-        // Vérifie si la caméra principale est disponible
         Camera mainCamera = Camera.main;
         if (mainCamera == null)
         {
@@ -198,30 +195,31 @@ public class CartesManager : MonoBehaviour
             return;
         }
 
-        // Définir la distance entre les cartes
-        float distanceEntreCartes = 2.5f; // Vous pouvez ajuster cette valeur en fonction de votre besoin
+        float distanceEntreCartes = 2.5f;
 
-        // Mémorise la position de la dernière carte instanciée
         Vector3 dernierePosition = mainCamera.transform.position + mainCamera.transform.forward * distanceSpawnDevantCamera;
 
         foreach (string carte in cartesObtenues)
         {
-            // Trouve l'index de la carte dans la liste originale
             int indexCarte = cartes.IndexOf(carte);
 
-            // Si l'index est valide dans le tableau de préfabs
             if (indexCarte >= 0 && indexCarte < prefabsCartes.Length)
             {
-                // Calcule la position devant la caméra avec un espace entre les cartes et un décalage en fonction de la dernière position
                 Vector3 positionDevantCamera = dernierePosition + Vector3.right * distanceEntreCartes;
 
-                // Instancie un nouvel objet à partir du préfab associé à la carte
                 GameObject nouvelleCarte = Instantiate(prefabsCartes[indexCarte], positionDevantCamera, Quaternion.identity);
 
-                // Mise à jour de la dernière position
                 dernierePosition = positionDevantCamera;
 
-                // Vous pouvez personnaliser la position, l'orientation, ou d'autres propriétés de l'objet ici
+                // Vérifie si la carte tirée est dans le dictionnaire et si le masque correspondant n'a pas été détruit
+                if (carteToMask.ContainsKey(carte) && !carteToMask[carte].destroyed)
+                {
+                    // Détruit le masque associé à la carte
+                    Destroy(carteToMask[carte].mask.gameObject);
+
+                    // Marque le masque comme détruit pour éviter une destruction ultérieure
+                    carteToMask[carte].destroyed = true;
+                }
             }
             else
             {
@@ -230,7 +228,6 @@ public class CartesManager : MonoBehaviour
         }
     }
 
-    // Fonction pour détruire les objets de carte existants
     private void DetruireCartesExistants()
     {
         GameObject[] cartesExistantes = GameObject.FindGameObjectsWithTag("Carte");
