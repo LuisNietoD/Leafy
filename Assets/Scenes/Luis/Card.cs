1972097757554a47077736b23c2f76349c84c431
LuisNietoD <107162215+LuisNietoD@users.noreply.github.com>
2024-01-20 20:12:50 +0100

    Card Drag + Stack system

--- Assets/Scenes/Luis/Card.cs
+++ Assets/Scenes/Luis/Card.cs
@@ -1,83 +1,111 @@
 using UnityEngine;
 using System;
 using System.Collections;
+using Leafy.Manager;
+using Unity.VisualScripting;
 
 namespace Leafy.Objects
 {
     public class Card : MonoBehaviour
     {
-        public float rotationForce = 0.1f;
-        public float rotationSpeed = 5.0f;
-        public float movementSpeed = 5.0f;
-        private GameObject shadow;
-        public float shadowOffsetY = 1;
-        public float shadowOffsetX = 1;
-        public float maxRotation;
+        private float movementSpeed = 30.0f;
+        private float offsetY = 0.5f;
+        private float offsetZ = 0.1f;
         
-        Vector3 lastMousePos;
-        public Vector3 mouseDelta
+        private Collider2D collider;
+        
+        private Card parent;
+        private Card child;
+
+
+        private void Awake()
         {
-            get
+            collider = GetComponent<Collider2D>();
+        }
+        
+        private void Update()
+        {
+            //Move the card with a little delay
+            if(parent != null)
             {
-                return Input.mousePosition - lastMousePos;
+                Vector3 targetPosition = parent.transform.position;
+                targetPosition.y -= offsetY;
+                Vector2 position = Vector2.Lerp(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+                float z = parent.transform.position.z - offsetZ;
+                transform.position = new Vector3(position.x, position.y, z);
             }
         }
 
-        void Start()
+        /// <summary>
+        /// Change all necessary value to make a card the main dragged object
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 SetDragged()
         {
-            lastMousePos = Input.mousePosition;
-            shadow = transform.GetChild(0).gameObject;
-        }
-        
-        
-        private void OnMouseDrag()
-        {
-            drag = true;
-            Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPosition.z = transform.position.z;
-            transform.position = Vector3.Lerp(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+            if(parent != null)
+                parent.child = null;
+            parent = null;
+                
+            ChangeStackCollider(false);
             
-            Vector3 moveRotation = new Vector3(0, 0, 0);
-            moveRotation.y = Mathf.Clamp(rotationForce * mouseDelta.x, -70f, 70f);
-            moveRotation.x = Mathf.Clamp(rotationForce/2 * mouseDelta.y, -70f, 70f);
-            moveRotation.z = 0;
-            Quaternion targetRotation = Quaternion.Euler(moveRotation);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x,
-                transform.eulerAngles.y, 0);
-
-            float rawRotationY = transform.eulerAngles.y;
-            if (rawRotationY > 180)
-            {
-                rawRotationY = -180 + (rawRotationY - 180);
-            }
-            
-            float rawRotationX = transform.eulerAngles.x;
-            if (rawRotationX > 180)
-            {
-                rawRotationX = -180 + (rawRotationX - 180);
-            }
-            shadow.transform.localPosition = new Vector3(-shadowOffsetX * (rawRotationY / maxRotation), 
-                -shadowOffsetY * (rawRotationX / maxRotation), 1f);
-            
-            lastMousePos = Input.mousePosition; 
+            return transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
-        private bool drag;
-
-        private void LateUpdate()
+        /// <summary>
+        /// Change all necessary value to drop the card
+        /// </summary>
+        /// <param name="card"></param>
+        public void Drop(Card card)
         {
-            if (!Input.GetMouseButton(0))
-            {
-                drag = false;
-            }
+            Vector3 p = transform.position;
+            p.z = 0;
+            transform.position = p;
+            parent = GetLastCard(card);
+            if(parent != null)
+                parent.child = this;
+            ChangeStackCollider(true);
+        }
+
+        
+        /// <summary>
+        /// Return the last card of the stack
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
+        public Card GetLastCard(Card card)
+        {
+            if (card == null)
+                return null;
+            if (card.child == null)
+                return card;
+            return GetLastCard(card.child);
+        }
+
+        /// <summary>
+        /// Put all the stack on front view when dragged
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
+        public Card FrontViewAllCard(Card card)
+        {
+            Vector3 p = transform.position;
+            p.z -= 9;
+            transform.position = p;
+            if (card.parent == null)
+                return card;
+            return FrontViewAllCard(card.parent);
+        }
+
+        /// <summary>
+        /// Change all the stack collider to avoid collision and raycast
+        /// </summary>
+        /// <param name="enable"></param>
+        public void ChangeStackCollider(bool enable)
+        {
+            if(child != null)
+                child.ChangeStackCollider(enable);
             
-            if (!drag)
-            {
-                Vector3 moveRotation = new Vector3(0, 0, 0);
-                Quaternion targetRotation = Quaternion.Euler(moveRotation);
-                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, (rotationSpeed * 3) * Time.deltaTime);
-            }
+            collider.enabled = enable;
         }
     }
 }
--- Assets/Scenes/Luis/GameManager.cs
+++ Assets/Scenes/Luis/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using Leafy.Objects;
 using UnityEngine;
 
@@ -5,7 +6,66 @@ namespace Leafy.Manager
 {
     public class GameManager: MonoBehaviour
     {
-        public static Card cardInHand;
+        public LayerMask cardLayer;
+        
+        private Card draggedCard;
+        private Card hoveredCard;
+        
+        private float draggedCardSpeed = 10;
+        private Vector3 offset;
+        private float frontViewZ;
+        
+        private void Update()
+        {
+            hoveredCard = RayTestCard();
+
+            //Grab the hovered card and drag it
+            if (Input.GetMouseButtonDown(0) && hoveredCard != null)
+            {
+                offset = hoveredCard.SetDragged();
+                draggedCard = hoveredCard;
+                draggedCard.FrontViewAllCard(draggedCard.GetLastCard(draggedCard));
+                hoveredCard = null;
+            }
+            //Drop the dragged card
+            else if (Input.GetMouseButtonUp(0) && draggedCard != null)
+            {
+                draggedCard.Drop(hoveredCard);
+                draggedCard = null;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            //Drag the card with a little delay
+            if (draggedCard != null)
+            {
+                Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+                Vector2 position = Vector2.Lerp(draggedCard.transform.position, targetPosition, draggedCardSpeed * Time.deltaTime);
+                float z = frontViewZ;
+                draggedCard.transform.position = new Vector3(position.x, position.y, z);
+            }
+        }
+
+        /// <summary>
+        /// Return the first card hit on the board
+        /// </summary>
+        /// <returns></returns>
+        public Card RayTestCard()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, cardLayer);
+
+            if (hit.collider != null)
+            {
+                Card card = hit.collider.GetComponent<Card>();
+                if (card != null)
+                {
+                    return card;
+                }
+            }
+            return null;
+        }
     }
 }
 
